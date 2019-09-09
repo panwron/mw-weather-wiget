89d83f73a999db78f04c38cd82b848a99d4d948f
Maciek Wrona <panwron@gmail.com>
2019-09-09 20:17:08 +0200

    set app presentation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from "react";
 import MwWeatherApp from "./components/mw-weather-widget";
 
 function App() {
-  return <MwWeatherApp />;
+  return (
+    <React.Fragment>
+      <div style={{ display: "inline-block", width: "50%" }}>
+        <MwWeatherApp />
+      </div>
+      <div
+        style={{
+          display: "inline-block",
+          width: "25%",
+          float: "right"
+        }}
+      >
+        <MwWeatherApp />
+      </div>
+
+      <div style={{ display: "block", width: "100%" }}>
+        <MwWeatherApp />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
